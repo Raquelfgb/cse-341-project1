0b5bf85df26c2e8dc4c39852f4e3b6d9809a0865
Raquelfgb <kelkinha@hotmail.com>
2024-05-01 18:04:13 -0600

    fix project1

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require("express");
-
-const mongodb = require("./data/database")
+const express = require('express');
+const cors = require('cors');
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use("/", require("./routes"));
+app
+.use(cors())
+.use('/', require('./routes'))
+;
 
 mongodb.initDb((err) => {
-    if(err) {
-        console.log(err);
-    }
-    else {
-        app.listen(port, () => (console.log(`Database is listening and node Running on port ${port}`)));
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => console.log(`Database is listening and node Running on port ${port}`));
+  }
 });
