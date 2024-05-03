5bef1bd2f3fa766e1a135c886bd53307bb7df0f2
Raquelfgb <kelkinha@hotmail.com>
2024-05-02 21:04:44 -0600

    add temple class

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const router = require('express').Router();
+const temple = require('./temple');
 
 // router.get('/', (req, res) => {
 //   res.send('Hello World');
 // });
 router.use('/',require('./swagger'));
 router.use('/contacts', require('./contacts'));
+routes.use('/temples', temple);
+routes.use(
+  '/',
+  (docData = (req, res) => {
+    let docData = {
+      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
+    };
+    res.send(docData);
+  })
+);
 
 module.exports = router;
